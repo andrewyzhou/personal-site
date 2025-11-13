b1c61b306ac0bba538e1ec1743f6bfe8e03d362d
Andrew Zhou <andrewzhou@berkeley.edu>
2025-11-12 21:07:56 -0800

    Add rewrite rule for wave-poser subdomain

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/wave-poser/:path*',
+        destination: 'https://wave-poser-90ywfw9bf-andrewyzhous-projects.vercel.app/wave-poser/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
